0b2f00fc364428aa492d47bb421fc92c8bf8f88f
Marc Morinville <marcmorinville@gmail.com>
2015-07-25 09:24:23 -0400

    PutItem, GetItem, UpdateItem and DeleteItem no longer require AttributeValues for their key descriptions
     - Also fixed a couple bugs related to incorrect context binding and names

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var AWS = require('aws-sdk');
 var Q = require('q');
+var marshaler = require('dynamodb-marshaler');
 
 function EasyDynamoDB(options) {
     this._dynamodb = new AWS.DynamoDB(options);
@@ -30,12 +31,20 @@ function run(func, callback) {
     }
 }
 
+function toDynamoDbFormat(toMarshal) {
+    return marshaler.marshalItem(toMarshal);
+}
+
+function fromDynamoDbFormat(toUnmarshal) {
+    return marshaler.unmarshalItem(toUnmarshal);
+}
+
 EasyDynamoDB.prototype.batchGetItem = function(params, callback) {
-    return run(this._dynamodb.batchGetItem.bind(this, params), callback);
+    return run(this._dynamodb.batchGetItem.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.batchWriteItem = function(params, callback) {
-    return run(this._dynamodb.batchWriteItem.bind(this, params), callback);
+    return run(this._dynamodb.batchWriteItem.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.createTable = function(params, callback) {
@@ -43,55 +52,59 @@ EasyDynamoDB.prototype.createTable = function(params, callback) {
 };
 
 EasyDynamoDB.prototype.deleteItem = function(params, callback) {
-    return run(this._dynamodb.deleteItem.bind(this, params), callback);
+
+    params.Key = toDynamoDbFormat(params.Key);
+
+    return run(this._dynamodb.deleteItem.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.deleteTable = function(params, callback) {
-    return run(this._dynamodb.deleteTable.bind(this, params), callback);
+    return run(this._dynamodb.deleteTable.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.describeTable = function(params, callback) {
-    return run(this._dynamodb.describeTable.bind(this, params), callback);
+    return run(this._dynamodb.describeTable.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.getItem = function(params, callback) {
-    return run(this._dynamodb.getItem.bind(this, params), callback);
+
+    params.Key = toDynamoDbFormat(params.Key);
+
+    return run(this._dynamodb.getItem.bind(this._dynamodb, params), callback);
 };
 
-EasyDynamoDB.prototype.listTable = function(params, callback) {
-    return run(this._dynamodb.listTable.bind(this, params), callback);
+EasyDynamoDB.prototype.listTables = function(params, callback) {
+    return run(this._dynamodb.listTables.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.putItem = function(params, callback) {
-    return run(this._dynamodb.putItem.bind(this, params), callback);
+
+    params.Item = toDynamoDbFormat(params.Item);
+
+    return run(this._dynamodb.putItem.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.query = function(params, callback) {
-    return run(this._dynamodb.query.bind(this, params), callback);
+    return run(this._dynamodb.query.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.scan = function(params, callback) {
-    return run(this._dynamodb.scan.bind(this, params), callback);
+    return run(this._dynamodb.scan.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.updateItem = function(params, callback) {
-    return run(this._dynamodb.updateItem.bind(this, params), callback);
-};
 
-EasyDynamoDB.prototype.query = function(params, callback) {
-    return run(this._dynamodb.query.bind(this, params), callback);
-};
+    params.Key = toDynamoDbFormat(params.Key);
 
-EasyDynamoDB.prototype.updateItem = function(params, callback) {
-    return run(this._dynamodb.updateItem.bind(this, params), callback);
+    return run(this._dynamodb.updateItem.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.updateTable = function(params, callback) {
-    return run(this._dynamodb.updateTable.bind(this, params), callback);
+    return run(this._dynamodb.updateTable.bind(this._dynamodb, params), callback);
 };
 
 EasyDynamoDB.prototype.waitFor = function(state, params, callback) {
-    return run(this._dynamodb.query.bind(this, state, params), callback);
+    return run(this._dynamodb.waitFor.bind(this._dynamodb, state, params), callback);
 };
 
 module.exports = EasyDynamoDB;
